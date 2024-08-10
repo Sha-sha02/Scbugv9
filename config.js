a58770b2760758d68e8c3116e405e3d11d6d1ea2
Ali Nawab <161608437+Alinawab006@users.noreply.github.com>
2024-08-10 22:06:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const chalk = require('chalk')
 // ganti info bot dibawah ini
 global.botName = "✯𝐃𝐀𝐑𝐊🖤𝐇𝐄𝐀𝐑𝐓✯𝘽𝙐𝙂 𝘽𝙊𝙏"
 global.ownerName = "✯𝐃𝐀𝐑𝐊🖤𝐇𝐄𝐀𝐑𝐓✯𝘽𝙐𝙂 𝘽𝙊𝙏"
-global.ownerBot = "306934306346"
-global.ownerNumber = ["306934306346"] 
+global.ownerBot = "923280444302"
+global.ownerNumber = ["923280444302"] 
 
 global.Auto_Typing = false // auto typing
 global.Auto_Recording = false // auto recording
